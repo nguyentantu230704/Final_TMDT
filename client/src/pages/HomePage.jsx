f2e4f6a0fedf57bdbb685605930db68e0a2edb6d
Nguyễn Tấn Tú <nguyentantu230704@gmail.com>
2025-11-09 00:32:14 +0700

    Thêm chatbot tawkto

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,67 +1,83 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Link } from "react-router-dom"
-import { ChevronRight } from "react-feather"
-import { categories, sliderItems } from '@/dummydata'
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ChevronRight } from "react-feather";
+import { categories, sliderItems } from "@/dummydata";
 
-import Button from "@/components/Button"
-import Container from "@/components/Container"
-import CategoryList from "@/ui/CategoryList"
-import ProductList from "@/ui/ProductList"
-import Newsletter from "@/ui/Newsletter"
-import Slider from "@/ui/Slider"
-import api from '../api'
-import { CartContext, UserContext } from "@/App"
-import Carousel from '../components/Carousel'
+import Button from "@/components/Button";
+import Container from "@/components/Container";
+import CategoryList from "@/ui/CategoryList";
+import ProductList from "@/ui/ProductList";
+import Newsletter from "@/ui/Newsletter";
+import Slider from "@/ui/Slider";
+import api from "../api";
+import { CartContext, UserContext } from "@/App";
+import Carousel from "../components/Carousel";
+
+//import mới
+import TawkTo from "../components/TawkTo";
 
 export default function HomePage() {
-	const {user} = useContext(UserContext)
-	const {cartDispatch} = useContext(CartContext)
-	const [products, setProducts] = useState([])
+  const { user } = useContext(UserContext);
+  const { cartDispatch } = useContext(CartContext);
+  const [products, setProducts] = useState([]);
 
-	useEffect(() => {
-		(async () => {
-			const resp = await api.fetchProducts("",true)
-			console.log(resp)
-			if (resp.status !== "error") {
-				setProducts(resp)
-			}
-		})()
-	}, [])
+  useEffect(() => {
+    (async () => {
+      const resp = await api.fetchProducts("", true);
+      console.log(resp);
+      if (resp.status !== "error") {
+        setProducts(resp);
+      }
+    })();
+  }, []);
 
-  const addToCart = async (product, quantity=1) => {
-		if (user) {
-			const resp = await api.addProductsToCart([{productID: product._id, quantity}])
-			if (resp.status === "ok") {
-				cartDispatch({type: "ADD_PRODUCTS", payload: [{...product, quantity}]})
-			}
-		} else {
-			cartDispatch({type: "ADD_PRODUCTS", payload: [{...product, quantity}]})
-		}
-	}
-	
-	return (
-		<main>
-			<section>
-				<Carousel slides={sliderItems} />
-			</section>
+  const addToCart = async (product, quantity = 1) => {
+    if (user) {
+      const resp = await api.addProductsToCart([
+        { productID: product._id, quantity },
+      ]);
+      if (resp.status === "ok") {
+        cartDispatch({
+          type: "ADD_PRODUCTS",
+          payload: [{ ...product, quantity }],
+        });
+      }
+    } else {
+      cartDispatch({
+        type: "ADD_PRODUCTS",
+        payload: [{ ...product, quantity }],
+      });
+    }
+  };
 
-			<Container heading="Popular Categories">
-				<CategoryList categories={categories} />
-			</Container>
+  return (
+    <main>
+      <section>
+        <Carousel slides={sliderItems} />
+      </section>
 
-			<Container heading="Latest Arrivals">
-				<ProductList products={[...products].splice(0,4)} onAddToCart={addToCart} />
+      <Container heading="Popular Categories">
+        <CategoryList categories={categories} />
+      </Container>
 
-				<Link to="/products" className="flex justify-center">
-					<Button className="text-lg mt-6" link>
-						View More <ChevronRight className="ml-2" />
-					</Button>
-				</Link>
-			</Container>
+      <Container heading="Latest Arrivals">
+        <ProductList
+          products={[...products].splice(0, 4)}
+          onAddToCart={addToCart}
+        />
 
-			<section className="my-20">
-				<Newsletter />
-			</section>
-		</main>
-	)
+        <Link to="/products" className="flex justify-center">
+          <Button className="text-lg mt-6" link>
+            View More <ChevronRight className="ml-2" />
+          </Button>
+        </Link>
+      </Container>
+
+      <TawkTo />
+
+      <section className="my-20">
+        <Newsletter />
+      </section>
+    </main>
+  );
 }
