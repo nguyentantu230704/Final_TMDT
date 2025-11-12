af291b285c4ebf71bc99e931b0a3434e1075cb09
Nguyễn Tấn Tú <nguyentantu230704@gmail.com>
2025-11-12 21:32:02 +0700

    "fix"

--- client/src/api.js
+++ client/src/api.js
@@ -1,62 +1,62 @@
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 function setAccessToken(token) {
-  localStorage.setItem('token', token)
+  localStorage.setItem("token", token);
 }
 function getAccessToken() {
-  return localStorage.getItem('token')
+  return localStorage.getItem("token");
 }
 
 function setUser(user) {
-  localStorage.setItem('user', JSON.stringify(user))
+  localStorage.setItem("user", JSON.stringify(user));
 }
 function getUser() {
-  const user = localStorage.getItem('user')
-  return user ? JSON.parse(user) : null
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
 }
 
-async function registerUser({fullname, email, password}) {
-  const resp = await fetch(API_URL+"/auth/register", {
+async function registerUser({ fullname, email, password }) {
+  const resp = await fetch(API_URL + "/auth/register", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({fullname, email, password}),
-  })
-  return await resp.json()
+    body: JSON.stringify({ fullname, email, password }),
+  });
+  return await resp.json();
 }
 
-async function loginUser({email, password}) {
-  const resp = await fetch(API_URL+"/auth/login", {
+async function loginUser({ email, password }) {
+  const resp = await fetch(API_URL + "/auth/login", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({email, password}),
-  })
-  const data = await resp.json()
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await resp.json();
 
   if (data.accessToken) {
-    setAccessToken(data.accessToken)
-    await fetchUserDetails()
+    setAccessToken(data.accessToken);
+    await fetchUserDetails();
   }
-  return data
+  return data;
 }
 
 function logoutUser() {
-  localStorage.clear()
+  localStorage.clear();
 }
 
 async function createUserCart(products) {
-  const resp = await fetch(API_URL+"/carts", {
+  const resp = await fetch(API_URL + "/carts", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "x-access-token": getAccessToken(),
     },
-    body: JSON.stringify(products.length ? {products}: {}),
-  })
-  return await resp.json()
+    body: JSON.stringify(products.length ? { products } : {}),
+  });
+  return await resp.json();
 }
 
 async function getUserCart() {
@@ -66,17 +66,17 @@ async function getUserCart() {
   }
   const userID = user._id;
   try {
-    const resp = await fetch(API_URL+"/carts/"+userID, {
+    const resp = await fetch(API_URL + "/carts/" + userID, {
       headers: {
         "x-access-token": getAccessToken(),
-      }
+      },
     });
     if (!resp.ok) {
       return { products: [] };
     }
     const cart = await resp.json();
     if (cart && Array.isArray(cart.products)) {
-      cart.products = cart.products.map(product => ({
+      cart.products = cart.products.map((product) => ({
         id: product.productID?._id,
         title: product.productID?.title,
         price: product.productID?.price,
@@ -102,55 +102,55 @@ async function getUserCart() {
 // }
 
 async function addProductsToCart(products) {
-  const userID = getUser()._id
-  const resp = await fetch(API_URL+"/carts/"+userID, {
+  const userID = getUser()._id;
+  const resp = await fetch(API_URL + "/carts/" + userID, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
       "x-access-token": getAccessToken(),
     },
-    body: JSON.stringify({products}),
-  })
-  return await resp.json()
+    body: JSON.stringify({ products }),
+  });
+  return await resp.json();
 }
 
 async function removeProductFromCart(productID) {
-  return await patchCart(productID, 0)
+  return await patchCart(productID, 0);
 }
 
 async function patchCart(productID, quantity) {
-  const userID = getUser()._id
-  const resp = await fetch(API_URL+"/carts/"+userID, {
+  const userID = getUser()._id;
+  const resp = await fetch(API_URL + "/carts/" + userID, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
       "x-access-token": getAccessToken(),
     },
-    body: JSON.stringify({productID, quantity}),
-  })
-  return await resp.json()
+    body: JSON.stringify({ productID, quantity }),
+  });
+  return await resp.json();
 }
 
 async function clearCart() {
-  const resp = await fetch(API_URL+"/carts/clear", {
+  const resp = await fetch(API_URL + "/carts/clear", {
     method: "POST",
     headers: {
       "x-access-token": getAccessToken(),
     },
-  })
-  return await resp.json()
+  });
+  return await resp.json();
 }
 
 async function fetchUserDetails() {
-  const resp =  await fetch(API_URL+"/users/me", {
+  const resp = await fetch(API_URL + "/users/me", {
     headers: {
       "x-access-token": getAccessToken(),
-    }
-  })
+    },
+  });
 
-  if ( !resp.ok ) {
+  if (!resp.ok) {
     console.warn("Server error", resp.status);
-    return { status: "error", user: null};
+    return { status: "error", user: null };
   }
   const data = await resp.json();
   const status = data?.status;
@@ -178,14 +178,17 @@ async function fetchUserDetails() {
   }
 }
 
-async function fetchProducts(category, newArrivals=false) {
-  let query = `new=${newArrivals ? "true" : "false"}${category ? "&category="+category : ""}`
-  const resp = await fetch(API_URL+"/products?"+query)
-  return await resp.json()
+async function fetchProducts(category, newArrivals = false) {
+  // nếu ko truyền tham số thứ 2, mặc định = false
+  let query = `new=${newArrivals ? "true" : "false"}${
+    category ? "&category=" + category : ""
+  }`;
+  const resp = await fetch(API_URL + "/products?" + query);
+  return await resp.json();
 }
 async function fetchProduct(id) {
-  const resp = await fetch(API_URL+"/products/"+id)
-  return await resp.json()
+  const resp = await fetch(API_URL + "/products/" + id);
+  return await resp.json();
 }
 
 // async function proceedCheckout() {
@@ -199,50 +202,53 @@ async function fetchProduct(id) {
 // }
 
 async function proceedCheckout() {
-  const resp = await fetch(API_URL+"/payment/create-paypal-order", {
+  const resp = await fetch(API_URL + "/payment/create-paypal-order", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "x-access-token": getAccessToken(),
     },
-  })
-  return await resp.json()
+  });
+  return await resp.json();
 }
 
 // on production create the order using stripe webhooks
 async function createOrder(products, amount, address) {
-  const resp = await fetch(API_URL+"/orders", {
+  const resp = await fetch(API_URL + "/orders", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "x-access-token": getAccessToken(),
     },
-    body: JSON.stringify({ 
-      products: products.map(p => ({productID: p.id, quantity: p.quantity})), 
-      amount, 
-      address 
+    body: JSON.stringify({
+      products: products.map((p) => ({
+        productID: p.id,
+        quantity: p.quantity,
+      })),
+      amount,
+      address,
     }),
-  })
-  return await resp.json()
+  });
+  return await resp.json();
 }
 
 async function fetchAllOrders() {
-  const userID = getUser()._id
-  const resp = await fetch(API_URL+"/orders/user/"+userID, {
+  const userID = getUser()._id;
+  const resp = await fetch(API_URL + "/orders/user/" + userID, {
     headers: {
       "x-access-token": getAccessToken(),
-    }
-  })
-  return await resp.json()
+    },
+  });
+  return await resp.json();
 }
 
 async function fetchOrderDetails(orderID) {
-  const resp = await fetch(API_URL+"/orders/"+orderID, {
+  const resp = await fetch(API_URL + "/orders/" + orderID, {
     headers: {
       "x-access-token": getAccessToken(),
-    }
-  })
-  return await resp.json()
+    },
+  });
+  return await resp.json();
 }
 
 export default {
@@ -264,4 +270,4 @@ export default {
   fetchAllOrders,
   fetchOrderDetails,
   // setAuthToken,
-}
+};
