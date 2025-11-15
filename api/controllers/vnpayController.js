cda6c5e90102311dff9e2f1f1b4b994d15d26a19
Thoaiiiiii <quocthoai2306@gmail.com>
2025-11-15 11:13:38 +0700

    sua FE_URL

--- api/controllers/vnpayController.js
+++ api/controllers/vnpayController.js
@@ -5,7 +5,6 @@ const Cart = require('../models/Cart.model');
 
 const createPaymentUrl = async ( req, res ) => {
     try {
-        // const { amount, address } = req.body;
         const { address } = req.body;
         const ipAddr = (req.ip || req.headers['x-forwarded-for'] || '127.0.0.1').replace(/^::ffff:/, '');
         const returnUrl = `${req.protocol}://${req.get('host')}/api/vnpay/return`;
@@ -39,10 +38,6 @@ const createPaymentUrl = async ( req, res ) => {
         const paymentUrl = vnpayService.buildPaymentUrl({
             amount: Number(amount),
             orderId: String(orderId),
-            // cartItems: cart.products.map(item => ({
-            //     price: item.productID.price,
-            //     quantity: item.quantity
-            // })),
             orderInfo: String(orderInfo),
             ipAddr,
             returnUrl,
@@ -72,14 +67,10 @@ const handleReturn = async (req, res) => {
         const orderId = req.query.vnp_txnRef;
 
         const order = await Order.findOne({ orderId });
-        // if ( !order ) {
-        //     return res.status(404).json({
-        //         success: false,
-        //         message: 'Khong tìm thấy đơn hàng.'
-        //     });
-        // }
+
         if ( !order ) {
-            return res.redirect(`http://localhost:3000/orders?payment=notfound&orderId=${orderId}`)
+            // return res.redirect(`http://localhost:3000/orders?payment=notfound&orderId=${orderId}`)
+            return res.redirect(`${process.env.FE_URL}/orders?payment=notfound&orderId=${orderId}`)
         }
 
         if (result.isSuccess && result.isVerified) {
@@ -94,55 +85,20 @@ const handleReturn = async (req, res) => {
             }, { $set: { products: [] } });
 
             // res.json({ success: true, message: 'Thanh toán thành công!' });
-            res.redirect(`http://localhost:3000/orders?payment=success&orderId=${orderId}`);
+            // res.redirect(`http://localhost:3000/orders?payment=success&orderId=${orderId}`);
+            res.redirect(`${process.env.FE_URL}/orders?payment=success&orderId=${orderId}`);
         } else {
             order.paymentStatus = 'failed';
             await order.save();
 
             // res.status(400).json({ success: false, message: 'Sai chữ ký hoặc thất bại!' });
-            return res.redirect(`http://localhost:3000/orders?payment=failed&orderId=${orderId}`);
+            return res.redirect(`${process.env.FE_URL}/orders?payment=failed&orderId=${orderId}`);
         }
     } catch (error) {
         // res.status(500).json({ success: false, message: 'Lỗi xử lý VNPay return.' });
-        return res.redirect(`http://localhost:3000/orders?payment=error`);
+        return res.redirect(`${process.env.FE_URL}/orders?payment=error`);
     }
 };
-
-// const handleReturn = async (req, res) => {
-//   try {
-//     const result = vnpayService.verifyReturn(req.query);
-//     const orderId = req.query.vnp_TxnRef;
-
-    
-//     const order = await Order.findOne({ orderId });
-//     if (!order) {
-//       return res.status(404).json({ success: false, message: "Không tìm thấy đơn hàng." });
-//     }
-
-//     if (result.isSuccess && result.isVerified) {
-//       order.paymentStatus = "paid";
-//       order.vnp_TransactionNo = req.query.vnp_TransactionNo;
-//       await order.save();
-
-//       res.json({
-//         success: true,
-//         message: "Thanh toán thành công!",
-//         order,
-//       });
-//     } else {
-//       order.paymentStatus = "failed";
-//       await order.save();
-
-//       res.status(400).json({
-//         success: false,
-//         message: "Thanh toán thất bại hoặc sai chữ ký!",
-//       });
-//     }
-//   } catch (error) {
-//     console.error("Return error:", error);
-//     res.status(500).json({ success: false, message: "Lỗi xử lý VNPay return." });
-//   }
-// };
 
 
 module.exports = {
