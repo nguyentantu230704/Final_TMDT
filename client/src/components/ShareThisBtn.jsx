3e7e959eb7cf60e06603ae82c4ab5b432839986d
Nguyễn Tấn Tú <nguyentantu230704@gmail.com>
2025-11-16 13:42:46 +0700

    thêm token

--- client/src/components/ShareThisBtn.jsx
+++ client/src/components/ShareThisBtn.jsx
@@ -2,21 +2,19 @@ import { useEffect } from "react";
 
 export default function ShareButtons({ pageUrl }) {
   useEffect(() => {
-    // Kiểm tra nếu script chưa tồn tại thì mới tạo
     if (!document.getElementById("sharethis-script")) {
       const script = document.createElement("script");
       script.src =
-        "https://platform-api.sharethis.com/js/sharethis.js#property=YOUR_PROPERTY_KEY&product=inline-share-buttons";
-      script.id = "sharethis-script"; // đặt id để không thêm nhiều lần
+        "https://platform-api.sharethis.com/js/sharethis.js#property=6919686890a70be8f33c0801&product=inline-share-buttons";
+      script.id = "sharethis-script";
       script.async = true;
       document.body.appendChild(script);
     } else {
-      // Nếu script đã load, khởi tạo lại ShareThis để render div mới
       window.__sharethis__?.initialize();
     }
   }, []);
 
   return (
-    <div data-url={pageUrl} className="sharethis-inline-share-buttons"></div>
+    <div className="sharethis-inline-share-buttons" data-url={pageUrl}></div>
   );
 }
